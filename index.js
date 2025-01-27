61771ad0dd01d1f746c0675a05ac14635e1dbd22
Pranav M <pranavm265@gmail.com>
2025-01-27 18:13:34 +0530

    First API

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const app = require('express')();
 const PORT = 8000;
 
-app.listen(
-    PORT,
-    () => console.log(`Server live on http://localhost:${PORT}`)
+app.listen(PORT, 
+    () => console.log(`Server active on http://localhost:${PORT}`)
 );
+
+app.get('/status', (req, res) => {
+    res.status(200).send({
+        status: 'healthy'
+    })
+});
