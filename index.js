37959c9750e2ea8adc8896659ce6eccf5529bfd9
Pranav M <pranavm265@gmail.com>
2025-01-27 18:28:57 +0530

    Post Get Middleware

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
 const PORT = 8000;
+
+app.use(express.json());
 
 app.listen(PORT, 
     () => console.log(`Server active on http://localhost:${PORT}`)
@@ -7,6 +10,18 @@ app.listen(PORT,
 
 app.get('/status', (req, res) => {
     res.status(200).send({
-        status: 'healthy'
+        status: 'Healthy Get'
     })
+});
+
+app.post('/post-status', (req, res) => {
+    const {check} = req.body;
+    if(!check) {
+        res.status(418).send({
+            message: "Mwone check status post eyy"
+        })
+    }
+    res.send({
+        status: "Healthy Post"
+    });
 });
